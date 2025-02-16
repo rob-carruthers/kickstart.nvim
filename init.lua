b39993a4c67f8541455a5f1ebe211e01df13f9fb
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-02-16 21:56:42 +0000

    Changed autocomplete mapping

--- init.lua
+++ init.lua
@@ -806,8 +806,9 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
+          ['<TAB>'] = cmp.mapping.confirm { select = true },
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
           --['<CR>'] = cmp.mapping.confirm { select = true },
